841b87dbf26ae9d699bd98697c0fea1abcda2ccf
Nikita Shmakov <nikshmakov@gmail.com>
2026-08-02 18:26:15 +0300

    correct linux clipboard util

--- cli/src/apass/clipboard.py
+++ cli/src/apass/clipboard.py
@@ -5,8 +5,8 @@ import subprocess
 def copy(text: str) -> None:
     system = platform.system()
     if system == "Darwin":
-        subprocess.run("pbcopy", input=text, text=True)
+        subprocess.run("pbcopy", input=text, text=True, check=True)
     elif system == "Linux":
-        subprocess.run(["xclip", "-selection", "clipboard"], input=text, text=True)
+        subprocess.run("wl-copy", input=text, text=True, check=True)
     elif system == "Windows":
-        subprocess.run("clip", input=text, text=True, shell=True)
+        subprocess.run("clip", input=text, text=True, shell=True, check=True)
